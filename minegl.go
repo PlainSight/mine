098347247a0b7b99c407b48b13f8567c03595974
Alex Ogier <alexander.ogier@gmail.com>
2016-08-08 23:37:46 +1200

    add numbers

--- minegl.go
+++ minegl.go
@@ -3,6 +3,10 @@ package main
 import (
 	"log"
 	"runtime"
+	"bytes"
+	"image"
+	"image/draw"
+	_ "image/png"
 
 	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
@@ -18,6 +22,10 @@ const (
 	WIDTH int = 720
 	HALF float32 = 360
 	SCALE float32 = 45
+)
+
+var (
+	numbers uint32
 )
 
 func handleClick(w *glfw.Window, button glfw.MouseButton, action glfw.Action, mod glfw.ModifierKey) {
@@ -55,6 +63,9 @@ func setup() {
 		panic(err)
 	}
 
+	numbers = loadNumbersTexture()
+	defer gl.DeleteTextures(1, &numbers)
+
 	setupScene()
 
 	window.SetMouseButtonCallback(handleClick);
@@ -66,6 +77,35 @@ func setup() {
 	}
 }
 
+func loadNumbersTexture() uint32 {
+	imageBytes, _ := numbersPngBytes()
+
+	img, _, _ := image.Decode(bytes.NewReader(imageBytes))
+
+	rgba := image.NewRGBA(img.Bounds())
+
+	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
+
+	var texture uint32
+	gl.Enable(gl.TEXTURE_2D)
+	gl.GenTextures(1, &texture)
+	gl.BindTexture(gl.TEXTURE_2D, texture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexImage2D(
+		gl.TEXTURE_2D,
+		0,
+		gl.RGBA,
+		int32(rgba.Rect.Size().X),
+		int32(rgba.Rect.Size().Y),
+		0,
+		gl.RGBA,
+		gl.UNSIGNED_BYTE,
+		gl.Ptr(rgba.Pix))
+
+	return texture
+}
+
 func setupScene() {
 	gl.MatrixMode(gl.PROJECTION)
 	gl.LoadIdentity()
@@ -74,15 +114,42 @@ func setupScene() {
 func destroyScene() {
 }
 
+func drawNumber(x float32, y float32, w float32, number int) {
+
+	margin := w / 4
+
+	x1 := ((x + margin) - HALF) / HALF
+	y1 := (HALF - (y + margin)) / HALF
+	x2 := (((x - margin) + w) - HALF) / HALF
+	y2 := (HALF - ((y - margin) + w)) / HALF
+
+	gl.BindTexture(gl.TEXTURE_2D, numbers)
+
+	gl.Color4f(1, 1, 1, 1)
+
+	gl.Begin(gl.QUADS)
+
+	txmin := float32(number) / 10.0
+	txmax := float32(number + 1) / 10.0
+
+	gl.TexCoord2f(txmin, 0)
+	gl.Vertex3f(x1, y1, 1)
+	gl.TexCoord2f(txmax, 0)
+	gl.Vertex3f(x2, y1, 1)
+	gl.TexCoord2f(txmax, 1)
+	gl.Vertex3f(x2, y2, 1)
+	gl.TexCoord2f(txmin, 1)
+	gl.Vertex3f(x1, y2, 1)
+
+	gl.End()
+}
+
 func drawSquare(x float32, y float32, w float32, color int, count int) {
 
 	x1 := (x - HALF) / HALF
 	y1 := (HALF - y) / HALF
 	x2 := ((x + w) - HALF) / HALF
 	y2 := (HALF - (y + w)) / HALF
-
-	countBlockSize := 3.0 / HALF
-	countBlockSpace := 4.0 / HALF
 
 	//color 0 = white, 1 = blue, 2 = pink
 
@@ -106,22 +173,11 @@ func drawSquare(x float32, y float32, w float32, color int, count int) {
 	gl.Vertex3f(x2, y2, 1)
 	gl.Vertex3f(x1, y2, 1)
 
-	if color == 0 && count > 0 {
-		gl.Color4f(0, 0, 0, 1)
-
-		for i := 0; i < count; i++ {
-			fc := float32(i + 2)
-
-			y3 := (y1 + y2) / 2
-
-			gl.Vertex3f(x1 + (fc * countBlockSpace), y3, 1)
-			gl.Vertex3f(x1 + (fc * countBlockSpace) + countBlockSize, y3, 1)
-			gl.Vertex3f(x1 + (fc * countBlockSpace) + countBlockSize, y3 - countBlockSize, 1)
-			gl.Vertex3f(x1 + (fc * countBlockSpace), y3 - countBlockSize, 1)
-		} 
-	}
-
 	gl.End()
+
+	if color == 0 && count > 0 {
+		drawNumber(x, y, w, count)
+	}
 }
 
 func drawScene() {
